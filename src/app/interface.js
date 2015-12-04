e84eb4c888105f770424c27bd61c61e6f14ee764
Kyle Cho <kylecho.work@gmail.com>
2015-12-04 01:26:25 -0800

    Implement welcoming component with a form to add a book

--- src/app/interface.js
+++ src/app/interface.js
@@ -1,10 +1,14 @@
 angular.module('app.interface', [])
   .controller('InterfaceCtrl', function (BooksService) {
     var interface = this;
+    interface.welcomingMode = true;
+    interface.addingMode = false;
 
     interface.submit = function () {
       BooksService.addBook({title:interface.title, author:interface.author});
       interface.title = '';
       interface.author = '';
+      interface.addingMode = false;
+      interface.welcomingMode = true;
     };
   });
