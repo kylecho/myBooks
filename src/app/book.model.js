a754cdd4c36f65d7d87487172591aa4e69102f3d
Kyle Cho <kylecho.work@gmail.com>
2015-12-04 01:10:28 -0800

    Implement book adding feature with error handlings

--- src/app/book.model.js
+++ src/app/book.model.js
@@ -15,16 +15,29 @@ angular.module('app.books.model', [])
       }
     ];
 
-    function isDup () {
-
+    function isDup (book) {
+      for (var i = 0; i < books.length; i++) {
+        if (books[i].title.toUpperCase() === book.title.toUpperCase()) {
+          return true;
+        }
+      }
+      return false;
     }
 
     function getBooks () {
       return books;
     }
 
-    function addBook () {
-
+    function addBook (book) {
+      if (isDup(book)) {
+        alert('The book with the same title already exists in your collection. Please try again.');
+      } else if (!book.title) {
+        alert('Please enter the title of the book.');
+      } else if (!book.author) {
+        alert('Please enter the author of the book.');
+      } else {
+        books.push(book);
+      }
     }
 
     return {
--- src/app/interface.js
+++ src/app/interface.js
@@ -1,6 +1,10 @@
 angular.module('app.interface', [])
-  .controller('InterfaceCtrl', function () {
+  .controller('InterfaceCtrl', function (BooksService) {
     var interface = this;
 
-
+    interface.submit = function () {
+      BooksService.addBook({title:interface.title, author:interface.author});
+      interface.title = '';
+      interface.author = '';
+    };
   });
